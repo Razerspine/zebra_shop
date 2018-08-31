a2d780e556cb74e3e293e4c82a3f354f60bbc2ab
Ponomarenko Pavel <ponomarenko.pavel.web@gmail.com>
2018-08-31 16:35:28 +0300

    testing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function () {
+    return gulp.src('css/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+});
